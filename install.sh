88c9bb5aced7b1b1f6c105270790f883c8ebb868
Jeong Yong-uk <yong@company100.com>
2015-01-30 10:23:37 +0900

    Don't sync .gitmodules.

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-rsync --exclude "LICENSE" --exclude "README.md" --exclude ".git/" --exclude "install.sh" -aP . ~
+rsync --exclude ".gitmodules" --exclude "LICENSE" --exclude "README.md" --exclude ".git/" --exclude "install.sh" -aP . ~
