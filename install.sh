e9fc486552adb40a587c107b35bce8d85a4e11f2
Jeong Yong-uk <yong@company100.com>
2015-04-03 15:18:25 +0900

    Add .gitconfig.

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash
+
+git_username="`git config --global user.name`"
+git_email="`git config --global user.email`"
+
+# sync files
 rsync --exclude ".gitmodules" \
       --exclude ".gitignore" \
       --exclude "LICENSE" \
@@ -6,3 +11,15 @@ rsync --exclude ".gitmodules" \
       --exclude ".git/" \
       --exclude "install.sh" \
       -aP . ~
+
+# git config
+if [ -z "$git_username" ]; then
+    read -r -p "git user name: " git_username
+fi
+
+if [ -z "$git_email" ]; then
+    read -p "git user email: " git_email
+fi
+
+git config --global user.name "$git_username"
+git config --global user.email "$git_email"
