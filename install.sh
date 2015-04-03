b94d30947ea028190a0d633bc6865e2cde208ac2
Jeong Yong-uk <yong@company100.com>
2015-04-03 17:25:53 +0900

    Warn if the terminal does not support 256 colors.

--- install.sh
+++ install.sh
@@ -17,3 +17,10 @@ fi
 
 git config --global user.name "$git_username"
 git config --global user.email "$git_email"
+
+# warn
+RED="\e[31m"
+RESET="\e[0m"
+if [ `tput colors` -ne 256 ]; then
+    echo -e $RED"This terminal does not support 256 color. 'tput colors' for more detail"$RESET
+fi
