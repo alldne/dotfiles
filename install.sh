71c34a079c193e4affb82466dec07e4b20b7d151
Jeong Yong-uk <ywj1022@gmail.com>
2015-11-23 15:42:53 +0900

    Change default shell to bash if it isn't

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/env bash
 
+YELLOW="\033[33m"
+RESET="\033[0m"
+
+if [ $SHELL != "/bin/bash" ]
+then
+    echo -e $YELLOW"You're now using $SHELL as a default. Trying to change it to bash."$RESET
+    chsh -s /bin/bash
+    echo -e $YELLOW"You should reopen the session to see the change."$RESET
+fi
+
 git_username="`git config --global user.name`"
 git_email="`git config --global user.email`"
 
