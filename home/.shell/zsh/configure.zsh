366b5e3c449207171b0fcc8ce8373bdddb8e0988
Jeong Yong-uk <ywj1022@gmail.com>
2015-11-30 21:24:05 +0900

    source nvm, rvm, .zshrc.local

--- home/.shell/zsh/configure.zsh
+++ home/.shell/zsh/configure.zsh
@@ -1,3 +1,7 @@
 source ~/.shell/aliases.sh
 source ~/.shell/exports.sh
 source ~/.shell/look.sh
+
+source-if-exists ~/.nvm/nvm.sh
+source-if-exists ~/.rvm/scripts/rvm
+source-if-exists ~/.zshrc.local
