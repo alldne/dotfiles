ca2746472e48c5f2da6018f4ec8cc04e80f1d8a1
Jeong Yong-uk <ywj1022@gmail.com>
2015-12-04 23:34:44 +0900

    Make git log not disappear after q in zsh

--- home/.shell/zsh/configure.zsh
+++ home/.shell/zsh/configure.zsh
@@ -5,3 +5,6 @@ source ~/.shell/look.sh
 source-if-exists ~/.nvm/nvm.sh
 source-if-exists ~/.rvm/scripts/rvm
 source-if-exists ~/.zshrc.local
+
+# https://www.reddit.com/r/zsh/comments/2n2ikk/in_bash_when_i_run_git_log_the_output_is/
+export LESS=-FRSX
