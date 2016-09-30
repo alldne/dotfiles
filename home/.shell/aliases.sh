5dae3b4121d8cb867b5d5b77bcbc06f1cdb1672d
Yonguk Jeong <yonguk.jeong@hpcnt.com>
2016-09-30 11:02:44 +0900

    Add osx alert

--- home/.shell/aliases.sh
+++ home/.shell/aliases.sh
@@ -5,7 +5,13 @@ alias l='ls -CF'
 
 # Add an "alert" alias for long running commands. Use like so:
 #   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+if [ -n "`~/.shell/scripts/is_mac`" ]; then
+    function alert { \
+        osascript -e "tell app \"System Events\" to display dialog \"$1\"" \
+    }
+else
+    alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+fi
 
 alias subl='sublime_text'
 
