ea5a018706deb0716b97433ebf1e8adce1ec4a73
Yonguk Jeong <yonguk.jeong@hpcnt.com>
2017-12-12 19:38:58 +0900

    Add swiftenv support

--- home/.shell/zsh/configure.zsh
+++ home/.shell/zsh/configure.zsh
@@ -7,5 +7,7 @@ source-if-exists ~/.nvm/nvm.sh
 source-if-exists ~/.rvm/scripts/rvm
 source-if-exists ~/.zshrc.local
 
+#eval "$(swiftenv init -)"
+
 # https://www.reddit.com/r/zsh/comments/2n2ikk/in_bash_when_i_run_git_log_the_output_is/
 export LESS=-FRSX
