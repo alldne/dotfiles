99c5fbc6230bc8815a90f99a641195debafedbbf
Jeong Yonguk <ywj1022@gmail.com>
2018-06-18 20:21:53 +0900

    Revise deps-mac.sh

--- deps-mac.sh
+++ deps-mac.sh
@@ -1,27 +1,68 @@
 #!/usr/bin/env bash
 
-if [ ! -s /Applications/Xcode.app ]
-then
-  echo "Install Xcode first"
-  exit 1
-fi
+# https://stackoverflow.com/a/28938235
+# Reset
+RESET='\033[0m'       # Text Reset
 
-# check if git is installed (included in xcode command line tool)
-which -s git
-if [[ $? != 0 ]]; then
-    echo "Install git first (xcode command line tool)"
-    exit 1
-fi
+# Regular Colors
+BLACK='\033[0;30m'        # Black
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+YELLOW='\033[0;33m'       # Yellow
+BLUE='\033[0;34m'         # Blue
+PURPLE='\033[0;35m'       # Purple
+CYAN='\033[0;36m'         # Cyan
+WHITE='\033[0;37m'        # White
+
+function __system {
+    echo -e $GREEN$*$RESET
+}
+
+function assert-deps {
+    command -v $1 >/dev/null 2>&1 || { echo >&2 "install ""$1"" first"; exit 1; }
+    __system '[OK]' $1
+}
+
+function brew-install-if-needed {
+    if brew ls $1 > /dev/null; then
+        __system '[OK]' $1
+    else
+        brew install $1
+    fi
+}
+
+assert-deps xcodebuild
+assert-deps git
+
+__system 'Current xcode path'
+xcode-select -p
 
 # install brew
-which -s brew
-if [[ $? != 0 ]]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
+if command -v brew > /dev/null; then
     brew update
+else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install wget
-brew install zsh zsh-completions
-brew install tmux
-brew install vim
+assert-deps brew
+
+brew-install-if-needed wget
+brew-install-if-needed zsh
+brew-install-if-needed zsh-completions
+brew-install-if-needed tmux
+brew-install-if-needed vim
+brew-install-if-needed ag
+
+if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+    __system "Generate ssh key"
+    ssh-keygen
+else
+    __system "[OK] ssh key under $HOME/.ssh"
+fi
+
+if [ ! -d "/Applications/Visual Studio Code.app" ]; then
+    __system "Install Visual Studio Code"
+    open https://code.visualstudio.com/docs/setup/mac
+else
+    __system "[OK] Visual Studio Code"
+fi
