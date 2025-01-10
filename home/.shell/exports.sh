a2d49fbfedcba732b58d39f92d0edad75f2a2c8a
Yonguk Jeong <ywj1022@gmail.com>
2025-01-10 10:10:42 +0900

    Add homebrew ruby to PATH

--- home/.shell/exports.sh
+++ home/.shell/exports.sh
@@ -3,9 +3,13 @@ export PATH=$PATH:~/.shell/scripts
 
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
-export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH" # For m1
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:$PATH" # For m1
 export PATH=$PATH:$HOME/.cargo/bin
+
+if [ -d "/opt/homebrew/opt/ruby/bin" ]; then
+  export PATH=/opt/homebrew/opt/ruby/bin:$PATH
+  export PATH=`gem environment gemdir`/bin:$PATH
+fi
 
 export LC_ALL="en_US.UTF-8"
 
